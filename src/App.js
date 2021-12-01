cc17e0237364740dc85178fb33ec16f42ec26577
Kyle <Kyle.Flavin@gmail.com>
2021-11-30 20:18:01 -0800

    Add role permissions

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const authLink = setContext((_, {headers}) => {
   return {
     headers: {
       ...headers,
-      // 'x-hasura-admin-secret': process.env.X_HASURA_ADMIN_SECRET,
+      'x-hasura-admin-secret': process.env.X_HASURA_ADMIN_SECRET,
     },
   }
 })
